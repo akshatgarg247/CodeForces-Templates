4edef4637c750207dd4ec35bfff8c26fc31126aa
Akshat Garg <akshatgargdli@gmail.com>
2026-04-03 16:34:24 +0530

    Update Standard Template.cpp

--- Standard Template.cpp
+++ Standard Template.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
 
-  return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+    return 0;
 }
